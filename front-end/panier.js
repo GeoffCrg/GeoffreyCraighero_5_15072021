8cf43b1fb4fed336ccf4d8f1e21d5a9d18772ed9
Geoff Crg <g.craighero@gmail.com>
2021-07-21 19:20:51 +0200

    validation du formulaire regEx

--- front-end/panier.js
+++ front-end/panier.js
@@ -7,7 +7,6 @@ console.log(InLocalStorage);
 
 //selection de la class ou je vais inserer le code html
 const displayPanier = document.querySelector("#panier-recap");
-console.log(displayPanier);
 
 //Si le panier est vide
 if (InLocalStorage === null || InLocalStorage == 0) {
@@ -73,7 +72,6 @@ displayPanier.insertAdjacentHTML("beforeend", btnSuppAll);
 
 //selectionner la ref du boutton
 btnDelAll = document.querySelector(".btnSuppPanier");
-console.log(btnDelAll);
 
 //suppression de la key produit du localstorage
 
@@ -86,32 +84,187 @@ btnDelAll.addEventListener("click", (e) => {
   alert("Le panier a été vider");
 });
 
-
-
 //////////////////Calculer le prix total du panier/////////////////////////////
 
-let prixTotalCalcul =[];
+let prixTotalCalcul = [];
 
 ////prendre les prix du panier
 
-for(let p = 0; p < InLocalStorage.length ; p++){
+for (let p = 0; p < InLocalStorage.length; p++) {
   let prixPanier = InLocalStorage[p].prixProduit / 100;
 
-///mettre les prix du panier dans la variable  prixtotalclalcul
+  ///mettre les prix du panier dans la variable  prixtotalclalcul
   prixTotalCalcul.push(prixPanier);
-    console.log(prixTotalCalcul);
-
-   
 }
- //additioner les prix de prix total calcul
- const reducer = (accumulator, currentValue) => accumulator + currentValue
- const prixTotal = prixTotalCalcul.reduce(reducer,0);
- console.log(prixTotal)
+//additioner les prix de prix total calcul
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+const prixTotal = prixTotalCalcul.reduce(reducer, 0);
 
+//inserer dans le html
+const affichageprixTotal = `<div class ="affichagePrix">Le prix total est de : ${prixTotal} € </div>
+ `;
 
- //inserer dans le html
- const affichageprixTotal =`<div class ="affichagePrix">Le prix total est de : ${prixTotal} € </div>
- `
+//injection
+displayPanier.insertAdjacentHTML("beforeend", affichageprixTotal);
 
- //injection 
- displayPanier.insertAdjacentHTML("beforeend", affichageprixTotal);
+//Affichage du formulaire
+
+const addHTMLFormulaire = () => {
+  //Selection element du DOM pour le positionnement du html
+  const DOMFormulaire = document.querySelector("#containerFormulaire");
+
+  const displayFormulaire = `
+
+<h2>Validez votre formulaire pour passer la commande</h2>
+
+<form action="#">
+<label for="prenom">Prenom</label>
+<input type="text" id="prenom" name="prenom" required>
+
+<label for="nom">nom</label>
+<input type="text" id="nom" name="nom" required>
+
+<label for="adresse">adresse</label>
+<input type="text" id="adresse" name="adresse" required>
+
+<label for="ville">Ville</label>
+<input type="text" id="ville" name="ville" required>
+
+<label for="E-mail">E-mail</label>
+<input type="email" id="e-mail" name="E-mail" required>
+
+<button id="envoyerFormulaire" type="submit" name="evoyerFormulaire">Envoyer votre formulaire</button>
+
+</form>
+
+`;
+  DOMFormulaire.insertAdjacentHTML("beforeend", displayFormulaire);
+};
+
+addHTMLFormulaire();
+//addEventListener boutton
+//selection du boutton Envoyer le formulaire
+const btnSubmit = document.querySelector("#envoyerFormulaire");
+
+btnSubmit.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  //recuperation des valeurs du formulaire
+  const formulaireValues = {
+    prenom: document.querySelector("#prenom").value,
+    nom: document.querySelector("#nom").value,
+    adresse: document.querySelector("#adresse").value,
+    ville: document.querySelector("#ville").value,
+    email: document.querySelector("#e-mail").value,
+  };
+
+  /////////////////Validation du formulaire///////////////////
+  const regExPrenomNomVille = (value) => {
+    return /^[A-Za-z]{2,20}$/.test(value);
+  };
+
+  const regExAdresse = (value) => {
+    return /^[A-Za-z0-9\s]{2,50}$/.test(value);
+  };
+
+  const regExEmail = (value) => {
+    return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(value);
+  };
+
+  const textAlertForm = (value) => {
+    return `${value}: Chiffre et symboles ne sont pas autorisés \n Ne pas despasser 20 caractères Min 3 caractères`;
+  };
+
+  function controlePrenom() {
+    const checkPrenom = formulaireValues.prenom;
+    if (regExPrenomNomVille(checkPrenom)) {
+      console.log("OK");
+      return true;
+    } else {
+      alert(textAlertForm("prenom"));
+      return false;
+    }
+  }
+  function controleNom() {
+    const checkNom = formulaireValues.nom;
+    if (regExPrenomNomVille(checkNom)) {
+      return true;
+    } else {
+      alert(textAlertForm("nom"));
+      return false;
+    }
+  }
+  function controleVille() {
+    const checkVille = formulaireValues.ville;
+    if (regExPrenomNomVille(checkVille)) {
+      return true;
+    } else {
+      alert(textAlertForm("ville"));
+      return false;
+    }
+  }
+  function controleAdresse() {
+    const checkAdresse = formulaireValues.adresse;
+    if (regExAdresse(checkAdresse)) {
+      return true;
+    } else {
+      alert("Veuillez renseignez une adresse valide ");
+      return false;
+    }
+  }
+  function controleEmail() {
+    const checkEmail = formulaireValues.email;
+    if (regExEmail(checkEmail)) {
+      return true;
+    } else {
+      alert("Veuillez renseigner une adresse  email valide");
+      return false;
+    }
+  }
+  if (
+    controlePrenom() &&
+    controleNom() &&
+    controleVille() &&
+    controleAdresse() &&
+    controleEmail()
+  ) {
+    //Mettre l'objet formulaireValues dans le local storage
+    localStorage.setItem("formulaireValues", JSON.stringify(formulaireValues));
+  } else {
+    alert("Veuillez bien remplir le formulaire");
+  }
+
+  fetch("http://localhost:3000/api/cameras"),
+    {
+      method: "post",
+      body: JSON.stringify(formulaireValues),
+      Headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+  ///// mettre le formulaire et les produits du panier pour les envoyé vers le serveurs
+  ///envoie de l'objet au serveur
+  const sendOrder = {
+    formulaireValues,
+    InLocalStorage,
+  };
+  console.log(sendOrder);
+});
+const dataLocalStorage = localStorage.getItem("formulaireValues");
+// ///Garder les valeurs dans le formulaire
+// //Convertir la chaine de caractere en objet javascript
+const dataLocalStorageObjet = JSON.parse(dataLocalStorage);
+// console.log(dataLocalStorageObjet);
+
+// //mettre les values du local storage dans les champs du formulaire
+function GarderValues(input) {
+  document.querySelector(`#${input}`).value = dataLocalStorageObjet[input];
+}
+GarderValues("prenom");
+GarderValues("nom");
+GarderValues("adresse");
+GarderValues("ville");
+GarderValues("e-mail");
+
+///////////////////////////Fin/////////////////////////////////////////////////
